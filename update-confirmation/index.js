264d2417adcdb1923d571a0221d9687e9b23fc91
pfauchet <42136778+pfauchet@users.noreply.github.com>
2019-07-05 14:04:16 +0200

    Created UpdateConfirmation lambda
    Fetch data on a google speadsheet and push it in DynamoDB

---
+++ update-confirmation/index.js
@@ -0,0 +1,76 @@
+var { google } = require("googleapis");
+let privatekey = require("./privatekey.json");
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-3' });
+
+//process.env.HTTPS_PROXY = "http://127.0.0.1:9000/"
+
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  [
+    'https://www.googleapis.com/auth/spreadsheets'
+  ]
+);
+
+//authenticate request
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Successfully connected!");
+  }
+});
+
+exports.handler = (event, context, callback) => {
+  //Google Sheets API
+  let spreadsheetId = '1r3uN2UUx1E4k0QZqETvBXfeFEGqMkOrFtDab9PFToGc';
+  let sheetName = 'data!A2:Z1000'
+  let sheets = google.sheets('v4');
+
+  sheets.spreadsheets.values.get({
+    auth: jwtClient,
+    spreadsheetId: spreadsheetId,
+    range: sheetName
+  }, function (err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+    } else {
+      console.log('Results : ' + JSON.stringify(response.data));
+      for (let value of response.data.values) {
+        let code = value[0];
+        let surname = value[1];
+        let name = value[2];
+        console.log("code = " + code);
+
+        /*
+        * Création de l'objet dans DynamoDB
+        */
+        var insert_params = {
+          Item: {
+            code: code,
+            surname: surname,
+            name: name
+          },
+          TableName: 'CONFIRMATION_TABLE'
+        };
+
+        docClient.put(insert_params, function (err, data) {
+          if (err) {
+            console.log("Error happened with " + code + " : " + err)
+          } else {
+            console.log("Success with : " + code)
+          }
+        });
+      }
+      callback(null, response)
+    }
+  });
+};
+
+// this.handler(null, null, () => {
+//   console.log("done")
+// })
